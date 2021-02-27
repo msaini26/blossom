acf98673628c5f3994f9f4d85ab97f6d29a46bae
Mansi S <mansisaini26@gmail.com>
2021-02-27 20:16:07 +0000

    updated accordians

--- nicepage.js
+++ nicepage.js
@@ -1,3 +1,23 @@
+/* Animation for drop down menus on FAQs page */
+var fol = document.getElementsByClassName("foldingFAQ");
+var i;
+
+for (i = 0; i < fol.length; i++) {
+  fol[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var board = this.nextElementSibling;
+    if (board.style.display === "block") {
+      board.style.display = "none";
+    } else {
+      board.style.display = "block";
+    }
+  });
+}
+
+
+
+
+
 !function(t){function e(n){if(i[n])return i[n].exports;var o=i[n]={i:n,l:false,exports:{}};return t[n].call(o.exports,o,o.exports,e),o.l=true,o.exports}var i={};return e.m=t,e.c=i,e.d=function(t,i,n){if(!e.o(t,i))Object.defineProperty(t,i,{configurable:false,enumerable:true,get:n})},e.n=function(t){var i=t&&t.__esModule?function e(){return t.default}:function e(){return t};return e.d(i,"a",i),i},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="/Content/BundledScripts/",e(e.s=4870)}({105:function(t,e){var e=void 0,t=void 0;(function(){/*!
  * https://github.com/gilmoreorless/css-background-parser
  * Copyright © 2015 Gilmore Davidson under the MIT license: http://gilmoreorless.mit-license.org/
