04e1f1dd7745bf665b61853978c3e00106e946a6
Ishika Soni <ishikasoni50@gmail.com>
2021-02-28 05:42:11 +0000

    added best time api, restaurants, and beauty and accessories tab

--- peaktimeapi.js
+++ peaktimeapi.js
@@ -1,6 +1,6 @@
 var params = {
    'api_key_public': 'pub_0c68dae5600f4caaa2ac79eddab193b3',
-    'venue_id': 'ven_514d6e747130594f4d33475267593946674f4d446366714a496843',
+    'venue_id': 'ven_4d6f4554787a38336536755267593947775447744d7a554a496843',
     'day_int': 3
 }
 
@@ -9,5 +9,5 @@ $.ajax({
 "method": "GET"
 }).done(function (response) {
     console.log(response);
-    $("#results").html("The peak time is " + response["analysis"]["hour_analysis"][0]["hour"]);
+    $("#results2").html("The peak time is " + response["analysis"]["hour_analysis"][0]["hour"]);
 });
